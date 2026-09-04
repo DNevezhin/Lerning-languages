dd67aa3ddcd8eda4abb2759fb6703705d03d926d
DNevezhin <nevezhin2000@bk.ru>
2026-09-05 00:20:32 +0300

    Добавлена модель User, DbContext и первая миграция

---
+++ LerningLanguages/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LerningLanguages.Models;
+
+namespace LerningLanguages.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=app.db");
+        }
+    }
+}
---
+++ LerningLanguages/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace LerningLanguages.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+    }
+}
